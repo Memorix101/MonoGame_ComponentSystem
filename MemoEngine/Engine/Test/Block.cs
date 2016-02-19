32c3288d8d3ac4e5798c4d1ae9911695360fded1
Memorix101 <memorix101@gmail.com>
2016-02-19 21:31:03 +0100

    Enhanced Ogmo level loader

--- MemoEngine/Engine/Test/Block.cs
+++ MemoEngine/Engine/Test/Block.cs
@@ -4,44 +4,52 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MemoEngine;
 
-namespace MonoGame_Zombii
+namespace MemoEngine
 {
-    class Tomato : GameObjectComponent
+    class Block : GameObjectComponent
     {
-        float speed = 150f;
-        float movex;
 
-        public Tomato()
+        float pos_X;
+        float pos_Y;
+        int rect_X;
+        int rect_Y;
+
+        public Block(float x, float y, int rx, int ry)
         {
-
+            pos_X = x;
+            pos_Y = y;
+            rect_X = rx;
+            rect_Y = ry;
         }
 
         public override void Init()
         {
             Sprite sprite = new Sprite();
             gameObject.AddComponent(sprite);
-            gameObject.GetComponent<Sprite>().Load("Sprites/tomatoes01");
+            gameObject.GetComponent<Sprite>().Load("Sprites/tiles");
             gameObject.GetComponent<Sprite>().scale = 1;
-            gameObject.GetComponent<Sprite>().rect = new Rectangle(0, 50, 50, 45);
 
+            gameObject.GetComponent<Sprite>().rect = new Rectangle(rect_X, rect_Y, 32, 32);
             Transform transform = new Transform();
             gameObject.AddComponent(transform);
-            gameObject.GetComponent<Transform>().position = new Vector2(Screen.width / 2 - gameObject.GetComponent<Sprite>().rect.Width, Screen.height / 2 - gameObject.GetComponent<Sprite>().rect.Height);
+            gameObject.GetComponent<Transform>().position = new Vector2(pos_X, pos_Y);
 
+            /*
             RectCollider collider = new RectCollider();
             gameObject.AddComponent(collider);
             gameObject.GetComponent<RectCollider>().Set(gameObject.GetComponent<Sprite>(), gameObject.GetComponent<Transform>());
+            */
 
             SpriteRenderer render = new SpriteRenderer();
             gameObject.AddComponent(render);
-            gameObject.GetComponent<SpriteRenderer>().layer = 5;
+            gameObject.GetComponent<SpriteRenderer>().layer = 0;
             gameObject.GetComponent<SpriteRenderer>().Set(gameObject.GetComponent<Sprite>());
 
         }
 
         public override void Update(GameTime gameTime)
         {
-            movex = gameObject.GetComponent<Transform>().position.X -= 25 * gameTime.DeltaTime();
+
         }
     }
 }
--- MemoEngine/Engine/Utilities/OgmoLoader.cs
+++ MemoEngine/Engine/Utilities/OgmoLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Xml;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,38 +9,57 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
+using System.Xml.Linq;
 
 namespace MemoEngine
 {
     public class OgmoLoader
     {
+
+        StringBuilder output = new StringBuilder();
+
         public void xmlLoader(string name)
         {
-            XmlTextReader reader = new XmlTextReader("Content/OgmoLevel/" + name +".oel");
-            while (reader.Read())
-            {
-                switch (reader.Name)
-                {
-                    case "level": 
-                        Console.Write("<" + reader.Name);
-                        Console.WriteLine(">");
-                        break;
-                    case "LevelObjects": //Display the text in each element.
-                        Console.WriteLine(reader.Value);
-                        break;
-                    case "tile": //Display the end of the element.
-                        Console.WriteLine(reader.GetAttribute("x")  + " - " + reader.GetAttribute("y"));
-                        GameObject tile = new GameObject();
-                        tile.AddComponent(new Tile(float.Parse(reader.GetAttribute("x"))*32, float.Parse(reader.GetAttribute("y"))*32, int.Parse(reader.GetAttribute("tx")) * 32, int.Parse(reader.GetAttribute("ty"))*32));
-                        Scene._AddedGO.Add(tile);
-                        break;
-                }          
 
-                Console.ReadLine();
+            String path = "Content/OgmoLevel/" + name + ".oel";
+
+            XDocument xdoc = XDocument.Load(path);
+            StringBuilder result = new StringBuilder();
+
+            var lvl = xdoc.Descendants("level");
+
+            foreach(var o in lvl)
+            {
+                foreach(var p in o.Descendants("LevelObjects"))
+                {
+                    foreach(var t in p.Descendants("tile"))
+                    {
+                        //result.AppendLine("LO " + t.Attribute("x").Value);
+                        GameObject tile = new GameObject();
+                        tile.AddComponent(new Tile(float.Parse(t.Attribute("x").Value) * 32, float.Parse(t.Attribute("y").Value) * 32, int.Parse(t.Attribute("tx").Value) * 32, int.Parse(t.Attribute("ty").Value) * 32));
+                        Scene._AddedGO.Add(tile);
+                    }
+                }
+
+                foreach (var p in o.Descendants("Obstacle"))
+                {
+                    foreach (var t in p.Descendants("tile"))
+                    {
+                        //result.AppendLine("OB " + t.Attribute("x").Value);
+                        GameObject block = new GameObject();
+                        block.AddComponent(new Block(float.Parse(t.Attribute("x").Value) * 32, float.Parse(t.Attribute("y").Value) * 32, int.Parse(t.Attribute("tx").Value) * 32, int.Parse(t.Attribute("ty").Value) * 32));
+                        Scene._AddedGO.Add(block);
+                    }
+                }                
             }
+            
+            Console.WriteLine(result.ToString());
+
         }
     }
 }
+    
+
 
     
 
